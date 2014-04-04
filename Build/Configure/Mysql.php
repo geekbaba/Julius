ee988a73ae086c4b92f59097724dbeeec0513612
geekbaba <jianghao19890829@gmail.com>
2014-04-04 21:38:45 +0800

    初次上传

---
+++ Build/Configure/Mysql.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Mysql配置
+ *
+ * @author      Jianghao <geekbaba.j@gmail.com>
+ * @date 2014-4-2 下午2:58:46
+ * @version 1.0.0
+ * @copyright  Copyright 2012 Joincoding Technolcgies Co.,Ltd.
+ */
+class Configure_Mysql extends JObject{
+	
+	//MYSQL 主机设置
+	public $HOST = '127.0.0.1';
+	//MYSQL 端口设置
+	public $PORT = '3306';
+	//MYSQL 用户名
+	public $USER ='root';
+	//MYSQL 密码设置
+	public $PASSWORD = '123456';
+	//MYSQL 数据名	
+	public $DBNAME = 'test';
+	//MYSQL 字符设置
+	public $CHARSET = 'utf8';
+	//MySQL 链接方式(驱动方式)
+	public $DRIVERTYPE = 'PDO';
+	
+}
